8fc2d9d04fff51b73c5df6598cbda988162f98a0
alioudiarra <alioudiarrapro@gmail.com>
2026-04-17 14:58:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ eleves = [
      {"prenom": "Chahinez", "nom": "Boubekeur", "github": "mariabkr26", "groupe": "Groupe 1"},
     {"prenom": "Alake Faridath", "nom": "ABOGOURIN", "github": "faridathabogourin", "groupe": "Gr1"},
     {"prenom": "Nora", "nom": "Boukal", "github": "loic-prof"},
+     {"prenom": "aliou", "nom": "diarra", "github": "alioudiarra", "groupe": "4"},
 ]
     
 TEMPLATE = """
